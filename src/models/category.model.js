5779309ddba7119acea81bbe3dbd892b9ed5ea71
huyvomieu <huy040424@gmail.com>
2025-04-24 21:47:33 +0700

    Fix API category

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -1,6 +1,7 @@
 const Joi = require("joi");
 
 const CategoryModel = Joi.object({
+  Key: Joi.string(),
   Id: Joi.string().max(30),
   ImagePath: Joi.string(),
   Name: Joi.string().min(3).max(100).required(),
